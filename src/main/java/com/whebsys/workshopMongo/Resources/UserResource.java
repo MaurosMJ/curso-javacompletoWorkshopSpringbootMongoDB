29a7692547e47d5ebff4ab94be34d36dcf45c830
Mauros Milach Junior <mauros99milach@gmail.com>
2024-06-05 19:55:40 -0300

    feat: created userById method

--- src/main/java/com/whebsys/workshopMongo/Resources/UserResource.java
+++ src/main/java/com/whebsys/workshopMongo/Resources/UserResource.java
@@ -1,12 +1,15 @@
 package com.whebsys.workshopMongo.Resources;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.whebsys.workshopMongo.domain.User;
@@ -25,5 +28,11 @@ public class UserResource {
 		List<User> list = userService.findAll();
 		List<UserDTO> listDTO = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Optional<User>> findById(@PathVariable String id){
+		Optional<User> obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
