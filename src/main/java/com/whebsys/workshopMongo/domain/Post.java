1d687bb91194cd239b6f5537bf4123e53b76a2aa
Mauros Milach Junior <mauros99milach@gmail.com>
2024-06-06 15:41:29 -0300

    feat: Add comments to user posts (database seeding)

--- src/main/java/com/whebsys/workshopMongo/domain/Post.java
+++ src/main/java/com/whebsys/workshopMongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.whebsys.workshopMongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.whebsys.workshopMongo.dto.AuthorDTO;
+import com.whebsys.workshopMongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,7 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post () {
 		
@@ -73,6 +77,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -89,8 +101,6 @@ public class Post implements Serializable {
 			return false;
 		Post other = (Post) obj;
 		return Objects.equals(id, other.id);
-	}
-	
-	
+	}	
 	
 }
