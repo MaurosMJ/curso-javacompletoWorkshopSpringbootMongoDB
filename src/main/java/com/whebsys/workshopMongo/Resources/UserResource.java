bb04d1a9a2b3b4ec9348c22dcb4c63241f2d61c7
Mauros Milach Junior <mauros99milach@gmail.com>
2024-06-06 12:15:35 -0300

    feat: Created an endpoint to return users posts

--- src/main/java/com/whebsys/workshopMongo/Resources/UserResource.java
+++ src/main/java/com/whebsys/workshopMongo/Resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.whebsys.workshopMongo.domain.Post;
 import com.whebsys.workshopMongo.domain.User;
 import com.whebsys.workshopMongo.dto.UserDTO;
 import com.whebsys.workshopMongo.services.UserService;
@@ -59,5 +60,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = userService.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		Optional<User> obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj.get().getPosts());
 	}
 }
