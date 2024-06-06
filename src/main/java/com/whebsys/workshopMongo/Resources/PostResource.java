335223c2164894a8c45ce5e1aeccf3787e074984
Mauros Milach Junior <mauros99milach@gmail.com>
2024-06-06 12:32:30 -0300

    feat: obtaining a post by ID

---
+++ src/main/java/com/whebsys/workshopMongo/Resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.whebsys.workshopMongo.Resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.whebsys.workshopMongo.domain.Post;
+import com.whebsys.workshopMongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService postService;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<Optional<Post>> findById(@PathVariable String id){
+		Optional<Post> obj = postService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
---
+++ src/main/java/com/whebsys/workshopMongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.whebsys.workshopMongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.whebsys.workshopMongo.domain.Post;
+import com.whebsys.workshopMongo.repository.PostRepository;
+import com.whebsys.workshopMongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Optional<Post> findById (String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return Optional.ofNullable(obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")));
+	}
+	
+}
